42cb6f568fdcc9e6570c0e22986f4c963f24bcfd
Matt Wheeler <matt.s.wheeler@gmail.com>
2018-02-19 13:49:44 -0700

    num_bandits parameterized

--- Beta_MultiArmBandit_Simulation.py
+++ Beta_MultiArmBandit_Simulation.py
@@ -9,8 +9,14 @@ from matplotlib import pyplot as plt
 
 start = timeit.default_timer()
 
+#### Simulation Parameters ###
+# Number of Bandits supported 3, 4, 5, and 6
+num_bandits = 4
+N = 5000
+##############################
+
 class BetaBandit(object):
-    def __init__(self, num_options=4, prior=(1.0,1.0)):
+    def __init__(self, num_options=num_bandits, prior=(1.0,1.0)):
         self.trials = zeros(shape=(num_options,), dtype=int)
         self.successes = zeros(shape=(num_options,), dtype=int)
         self.num_options = num_options
@@ -48,16 +54,20 @@ class BetaBandit(object):
 
 
 def boolean_select(self):
-    theta_random = (random.random(),random.random(),random.random(),random.random())
+    # Create list of random feedback Theta values, based on the number of specified bandits
+    theta_random = []
+    for i in range(num_bandits):
+        theta_random.append(random.random())
+    # Check a random value against the random thetas for each bandit
     if random.random() > theta_random[choice]:
         return True
     else:
         return False
 
 
-N = 5000
-trials = zeros(shape=(N,4))
-successes = zeros(shape=(N,4))
+
+trials = zeros(shape=(N,num_bandits))
+successes = zeros(shape=(N,num_bandits))
 beta_full_frame = []
 theta_full_frame = []
 choices = []
@@ -85,13 +95,20 @@ theta_df = pd.DataFrame(theta_temp)
 theta_df = theta_df.reset_index(inplace=False)
 
 theta_df['Event'] = theta_df.index
-print(theta_df)
 
 # Transpose various data sources for final joins
-theta_df_trans =pd.melt(theta_df, id_vars='Event', value_vars=[0, 1, 2, 3], value_name='Sample_Theta')
+theta_df_trans =pd.melt(theta_df, id_vars='Event',  value_name='Sample_Theta')#, value_vars=[0, 1, 2, 3])
 theta_df_trans = theta_df_trans.rename(columns={'variable': 'Arm'})
 
-trials_df = pd.DataFrame(trials, columns =['Arm0','Arm1','Arm2','Arm3'])
+if num_bandits == 3:
+    trials_df = pd.DataFrame(trials, columns=['Arm0', 'Arm1', 'Arm2'])
+elif num_bandits == 4:
+    trials_df = pd.DataFrame(trials, columns =['Arm0','Arm1','Arm2','Arm3'])
+elif num_bandits == 5:
+    trials_df = pd.DataFrame(trials, columns =['Arm0','Arm1','Arm2','Arm3','Arm4'])
+elif num_bandits == 6:
+    trials_df = pd.DataFrame(trials, columns =['Arm0','Arm1','Arm2','Arm3','Arm4','Arm5'])
+
 trials_df['Event'] = trials_df.index
 choice_df = pd.DataFrame(choices, columns=['Event','Choice'])
 
@@ -99,8 +116,17 @@ beta_temp= pd.concat(beta_full_frame, axis=0)
 beta_df = pd.DataFrame(beta_temp, columns=['Arm', 'a', 'b','Event'])
 
 beta_full_df = pd.merge(beta_df, choice_df[['Event','Choice']], on=['Event'], how='left')
+
 # Merge each choice per event to the beta data
-beta_merge1 = pd.merge(beta_full_df, trials_df[['Event','Arm0','Arm1','Arm2','Arm3']], on=['Event'], how='left')
+if num_bandits == 3:
+    beta_merge1 = pd.merge(beta_full_df, trials_df[['Event','Arm0','Arm1','Arm2']], on=['Event'], how='left')
+elif num_bandits == 4:
+    beta_merge1 = pd.merge(beta_full_df, trials_df[['Event', 'Arm0', 'Arm1', 'Arm2','Arm3']], on=['Event'], how='left')
+elif num_bandits == 5:
+    beta_merge1 = pd.merge(beta_full_df, trials_df[['Event', 'Arm0', 'Arm1', 'Arm2','Arm3','Arm4']], on=['Event'], how='left')
+elif num_bandits == 6:
+    beta_merge1 = pd.merge(beta_full_df, trials_df[['Event', 'Arm0', 'Arm1', 'Arm2','Arm3','Arm4','Arm5']], on=['Event'], how='left')
+
 # Merge max sampled theta values to each event
 beta_final = pd.merge(beta_merge1, theta_df_trans[['Event','Arm','Sample_Theta']], on=['Event','Arm'], how='left')
 
@@ -109,10 +135,10 @@ beta_final.to_csv('Beta Distribution Data.csv', index=False)
 from pylab import *
 subplot(211)
 n = arange(N)+1
-plot(n, trials[:,0], label="Arm 0 - Vice Principals")
-plot(n, trials[:,1], label="Arm 1 - Young Pope")
-plot(n, trials[:,2], label="Arm 2 - The Deuce")
-plot(n, trials[:,3], label="Arm 3 - GoT")
+
+for k in range(num_bandits):
+    plot(n, trials[:, k], label="Arm %d" % k)
+
 legend()
 title('Theoretical HBO Shows - Successes per Arm (%i Simulated Events)' % N)
 xlabel("Number of trials")
